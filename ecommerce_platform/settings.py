9777e19b77882026a27fa92f3e4ba52c2c5aa055
malbhagemahesh <mahesh.malbhage7@gmail.com>
2026-07-07 18:59:23 +0530

    Configure settings for Render

--- ecommerce_platform/settings.py
+++ ecommerce_platform/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6i9)$^ny)e#rzan%16b$q*ra9&u%%luwmgem))_c3h0k!#0ly9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
