6811cac0617d88daf10071bbb62fa8d1ef8c44bc
malbhagemahesh <mahesh.malbhage7@gmail.com>
2026-07-07 19:30:07 +0530

    Use SQLite for deployment

--- ecommerce_platform/settings.py
+++ ecommerce_platform/settings.py
@@ -81,14 +81,21 @@ WSGI_APPLICATION = 'ecommerce_platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ecommerce_db',
+#         'USER': 'root',
+#         'PASSWORD': 'RootPassword123!',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecommerce_db',
-        'USER': 'root',
-        'PASSWORD': 'RootPassword123!',
-        'HOST': 'localhost',
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
